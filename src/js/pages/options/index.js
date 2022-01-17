8b21e68f3063d364c54dad5f642622d0a8d75b14
r2dev2 <ronak.badhe@gmail.com>
2022-01-16 21:26:41 -0800

    it mek empty chunk but at least it mek chunk

---
+++ src/js/pages/options/index.js
@@ -0,0 +1,16 @@
+import App from '../../../components/Options.svelte';
+import { displayMode } from '../../store.js';
+import { DisplayMode } from '../../constants.js';
+import 'smelte/src/tailwind.css';
+
+displayMode.set(DisplayMode.POPOUT);
+
+const app = new App({
+  target: document.body,
+  props: {
+  }
+});
+
+window.app = app;
+
+export default app;
--- vite.config.js
+++ vite.config.js
@@ -2,55 +2,73 @@ import { defineConfig } from 'vite';
 import browserExtension from 'vite-plugin-web-extension';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import copy from 'rollup-plugin-copy';
+import mpa from 'vite-plugin-mpa';
 import htmlTemplate from 'vite-plugin-html-template';
 import manifest from './src/manifest.json';
 import pkg from './package.json';
 import path from 'path';
 
-const emptyHtml = path.join(__dirname, 'src', 'empty.html');
+// const emptyHtml = path.join(__dirname, 'src', 'empty.html');
+const emptyHtml = 'src/empty.html';
 /** @type {(js: string) => string} */
-const htmlEntry = js => path.join(__dirname, 'src', 'js', 'pages', js);
+const htmlEntry = js => `src/js/pages/${js}.js`;
 
 export default defineConfig(({ mode }) => ({
-  root: 'src',
   build: {
-    outDir: path.resolve(__dirname, 'build'),
+    outDir: 'build',
     emptyOutDir: true,
-    minify: mode === 'production' ? 'terser' : 'esbuild'
+    minify: mode === 'production' ? 'terser' : 'esbuild',
+    rollupOptions: {
+      input: {
+        options: path.resolve(__dirname, 'src/js/pages/options/index.html')
+      }
+    }
   },
   plugins: [
-    browserExtension({
-      manifest: () => ({
-        ...manifest,
-        version: (process.env.VERSION ?? '') || pkg.version,
-        description: pkg.description
-      }),
-      assets: 'img',
-      watchFilePaths: [
-        path.resolve(__dirname, 'src/manifest.json')
-      ]
-    }),
+    // browserExtension({
+    //   manifest: () => ({
+    //     ...manifest,
+    //     version: (process.env.VERSION ?? '') || pkg.version,
+    //     description: pkg.description
+    //   }),
+    //   assets: 'img',
+    //   watchFilePaths: [
+    //     path.resolve(__dirname, 'src/manifest.json')
+    //   ]
+    // }),
     svelte({
       configFile: path.resolve(__dirname, 'svelte.config.js'),
       emitCss: false
     }),
-    copy({
-      targets: [{
-        src: 'build/manifest.json',
-        dest: 'build',
-        transform: content => JSON.stringify({
-          ...JSON.parse(content.toString()),
-          incognito: 'split'
-        }, null, 2),
-        rename: 'manifest.chrome.json'
-      }]
-    }),
+    // copy({
+    //   targets: [{
+    //     src: 'build/manifest.json',
+    //     dest: 'build',
+    //     transform: content => JSON.stringify({
+    //       ...JSON.parse(content.toString()),
+    //       incognito: 'split'
+    //     }, null, 2),
+    //     rename: 'manifest.chrome.json'
+    //   }]
+    // }),
+    // mpa({
+    //   scanDir: 'src/js/pages',
+    //   scanFile: 'index.js'
+    // }),
     htmlTemplate({
-      pagesDir: 'src',
       pages: {
-        options: { template: emptyHtml, entry: htmlEntry('options.js') },
-        background: { template: emptyHtml, entry: htmlEntry('background.js') }
+        options: {
+          template: 'src/empty.html',
+          entry: 'src/js/pages/options/index.js'
+        }
+        // options: { template: emptyHtml, entry: 'src/js/pages/options/index.js' }
+        // background: { template: emptyHtml, entry: htmlEntry('background') }
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 }));
